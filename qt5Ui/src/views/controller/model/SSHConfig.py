81303e5baa99f8128d87bef37b3b00ea2c196548
ArthurWuTW <lion811004@gmail.com>
2021-09-26 00:07:00 +0800

    SSHConfig

---
+++ qt5Ui/src/views/controller/model/SSHConfig.py
@@ -0,0 +1,22 @@
+class SSHConfig():
+    def __init__(self):
+        self.ip = None
+        self.port = None
+        self.username = None
+        self.password = None
+    def setIP(ip):
+        self.ip = ip
+    def setPort(port):
+        self.port = port
+    def setUsername(username):
+        self.username = username
+    def setPassword(password):
+        self.password = password
+    def getIP():
+        return self.ip
+    def getPort():
+        return self.port
+    def getUsername():
+        return self.username
+    def getPassword():
+        return self.password
