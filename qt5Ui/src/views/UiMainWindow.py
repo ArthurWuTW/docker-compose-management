6b5b9aaaecafb39810f6f449828e484fe4054ad4
ArthurWuTW <lion811004@gmail.com>
2021-09-25 21:58:43 +0800

    trigger method

--- qt5Ui/src/views/UiMainWindow.py
+++ qt5Ui/src/views/UiMainWindow.py
@@ -1,12 +1,21 @@
 from codeGen.Ui_MainWindowBase import Ui_MainWindowBase
+from UiDialogCreateRsaKey import UiDialogCreateRsaKey
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 class UiMainWindow(Ui_MainWindowBase):
+    def __init__(self):
+        super().__init__()
+        self.qDialogCreateRsaKey = None
+
     def setupUi(self, UiMainWindow):
         super().setupUi(UiMainWindow)
+        self.actionCreate_Rsa_Key.triggered.connect(self.showDialogCreateRsaKeyCallback)
 
-
-
+    def showDialogCreateRsaKeyCallback(self):
+        self.qDialogCreateRsaKey = QtWidgets.QDialog()
+        ui = UiDialogCreateRsaKey()
+        ui.setupUi(self.qDialogCreateRsaKey)
+        self.qDialogCreateRsaKey.show()
 
 if __name__ == "__main__":
     import sys
