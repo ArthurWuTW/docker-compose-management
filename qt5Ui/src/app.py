aa23335c0ab3dcbbc052faf3719893e52d6e51d7
ArthurWuTW <lion811004@gmail.com>
2021-09-25 22:50:27 +0800

    run app

--- qt5Ui/src/app.py
+++ qt5Ui/src/app.py
@@ -1,9 +1,32 @@
- 
-#          self.actionCreate_Rsa_Key.triggered.connect(self.window2)
-#         self.Dialog = None
- 
-#  def window2(self):
-#         self.Dialog = QtWidgets.QDialog()
-#         ui = Ui_Dialog()
-#         ui.setupUi(self.Dialog)
-#         self.Dialog.show()
+import sys
+import json
+class ConfigJsonParser():
+    def __init__(self):
+        self.data = {}
+    def parse(self, file):
+        with open(file) as jsonFile:
+            self.data = json.load(jsonFile)
+    def getJsonData(self):
+        return self.data
+    
+class SubmodulePackageRelativePathImporter():
+    def __init__(self):
+        self.parser = ConfigJsonParser()
+    def importSubmoduleDependencyPath(self, file):
+        self.parser.parse(file)
+        print(self.parser.getJsonData())
+        for path in self.parser.getJsonData()['packageRelativePath']:
+            sys.path.append(path)
+
+if __name__ == "__main__":
+    importer = SubmodulePackageRelativePathImporter()
+    importer.importSubmoduleDependencyPath('config.json')
+
+    from views.UiMainWindow import UiMainWindow
+    from PyQt5 import QtCore, QtGui, QtWidgets
+    app = QtWidgets.QApplication(sys.argv)
+    qWindow = QtWidgets.QMainWindow()
+    ui = UiMainWindow()
+    ui.setupUi(qWindow)
+    qWindow.show()
+    sys.exit(app.exec_())
