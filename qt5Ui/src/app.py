c505692bbf1579986e56f79fdab514ec57550359
ArthurWuTW <lion811004@gmail.com>
2021-09-26 09:21:36 +0800

    remove redundant code

--- qt5Ui/src/app.py
+++ qt5Ui/src/app.py
@@ -1,25 +1,6 @@
 import sys
-import json
-class ConfigParser():
-    def __init__(self):
-        self.data = {}
-    def parseJsonFile(self, file):
-        with open(file) as jsonFile:
-            self.data = json.load(jsonFile)
-    def getDictData(self):
-        return self.data
-    
-class PackagePathImporter():
-    def __init__(self):
-        self.configParser = ConfigParser()
-    def importSubmoduleDependencyPath(self, file):
-        self.configParser.parseJsonFile(file)
-        for path in self.configParser.getDictData()['packageRelativePath']:
-            sys.path.append(path)
 
 if __name__ == "__main__":
-    importer = PackagePathImporter()
-    importer.importSubmoduleDependencyPath('config.json')
 
     from views.UiMainWindow import UiMainWindow
     from PyQt5 import QtCore, QtGui, QtWidgets
