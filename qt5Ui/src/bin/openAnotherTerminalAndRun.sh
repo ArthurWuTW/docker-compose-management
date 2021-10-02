ca46384b3c8d7a6d39c8869fd75da8b17348e75a
ArthurWuTW <lion811004@gmail.com>
2021-10-02 10:07:58 +0800

    DISPLAY :0

--- qt5Ui/src/bin/openAnotherTerminalAndRun.sh
+++ qt5Ui/src/bin/openAnotherTerminalAndRun.sh
@@ -3,4 +3,5 @@
 MACHINE=$1
 LOGIN_USER=$2
 CONTAINER=$3
+export DISPLAY=:0
 xterm -hold -e ssh -t $MACHINE 'docker exec --env TERM=xterm-256color --interactive --tty --user '$LOGIN_USER' '$CONTAINER' bash -c "cd /home && /bin/bash -l" && /bin/bash'
